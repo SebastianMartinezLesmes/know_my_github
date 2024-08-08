1fac937666779b49252b4b2f5cde0a18b8a2d4b4
Lesmes <20rasputin03@gmail.com>
2024-08-07 20:20:02 -0500

    CREATE: graphic circle (in use)

--- front_angular/src/app/components/circle-data/circle-data.component.ts
+++ front_angular/src/app/components/circle-data/circle-data.component.ts
@@ -1,60 +1,59 @@
-import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { Component, Input, OnInit, AfterViewInit } from '@angular/core';
 import * as echarts from 'echarts';
 
 @Component({
-  selector: 'app-circle-data',
-  templateUrl: './circle-data.component.html',
-  styleUrls: ['./circle-data.component.scss'],
+    selector: 'app-circle-data',
+    templateUrl: './circle-data.component.html',
+    styleUrls: ['./circle-data.component.scss'],
 })
 export class CircleDataComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+    @Input() languages: { name: string, percentage: string }[] = [];
 
-  ngOnInit() {}
+    constructor() { }
 
-  ngAfterViewInit() {
-    this.initChart();
-  }
+    ngOnInit() {}
 
-  initChart() {
-    const chartDom = document.getElementById('main')!;
-    const myChart = echarts.init(chartDom);
-    const option = {
-      title: {
-        text: 'Languages Usage',
-        subtext: 'in percentage',
-        left: 'center'
-      },
-      tooltip: {
-        trigger: 'item'
-      },
-      legend: {
-        orient: 'vertical',
-        left: 'left'
-      },
-      series: [
-        {
-          name: 'Languages',
-          type: 'pie',
-          radius: '50%',
-          data: [
-            { value: 1048, name: 'JavaScript' },
-            { value: 735, name: 'HTML' },
-            { value: 580, name: 'CSS' },
-            { value: 484, name: 'TypeScript' },
-            { value: 300, name: 'Other' }
-          ],
-          emphasis: {
-            itemStyle: {
-              shadowBlur: 10,
-              shadowOffsetX: 0,
-              shadowColor: 'rgba(0, 0, 0, 0.5)'
-            }
-          }
-        }
-      ]
-    };
+    ngAfterViewInit() {
+        this.initChart();
+    }
 
-    option && myChart.setOption(option);
-  }
+    initChart() {
+        const chartDom = document.getElementById('main')!;
+        const myChart = echarts.init(chartDom);
+        const option = {
+            title: {
+                text: 'Languages Usage',
+                subtext: 'in percentage',
+                left: 'center'
+            },
+            tooltip: {
+                trigger: 'item',
+                formatter: '{b}: {d}%'
+            },
+            legend: {
+                orient: 'vertical',
+                left: 'left'
+            },
+            series: [
+                {
+                    name: 'Languages',
+                    type: 'pie',
+                    radius: '50%',
+                    data: this.languages.map(lang => ({
+                        value: parseFloat(lang.percentage),
+                        name: lang.name
+                    })),
+                    emphasis: {
+                        itemStyle: {
+                        shadowBlur: 10,
+                        shadowOffsetX: 0,
+                        shadowColor: 'rgba(0, 0, 0, 0.5)'
+                        }
+                    }
+                }
+            ]
+        };
+        option && myChart.setOption(option);
+    }
 }
