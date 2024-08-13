05fd64542b7d6bbbfe48898295ec678863171764
Sebastian Martinez Lesmes <120604491+SebastianMartinezLesmes@users.noreply.github.com>
2024-08-13 16:49:52 -0500

    Update components.module.ts
    add component

--- front_angular/src/app/components/components.module.ts
+++ front_angular/src/app/components/components.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { IonicModule } from '@ionic/angular'; // Import IonicModule
+import { NgxEchartsModule } from 'ngx-echarts'; // Import NgxEchartsModule
 
 import { FrontendGraphicComponentComponent } from "./frontend-graphic-component/frontend-graphic-component.component";
 import { BackendGraphicComponentComponent } from "./backend-graphic-component/backend-graphic-component.component";
 import { DatabaseGraphicComponentComponent } from "./database-graphic-component/database-graphic-component.component";
 import { EchartsComponent } from "./echarts/echarts.component";
+import { CircleDataComponent } from "./circle-data/circle-data.component";
+import { BarDataComponent } from './bar-data/bar-data.component';
 
 @NgModule({
     declarations: [
@@ -13,16 +16,21 @@ import { EchartsComponent } from "./echarts/echarts.component";
         BackendGraphicComponentComponent,
         DatabaseGraphicComponentComponent,
         EchartsComponent,
+        CircleDataComponent,
+        BarDataComponent,
     ],
     exports: [
         FrontendGraphicComponentComponent,
         BackendGraphicComponentComponent,
         DatabaseGraphicComponentComponent,
         EchartsComponent,
+        CircleDataComponent,
+        BarDataComponent,
     ],
     imports: [
         CommonModule,
         IonicModule,
+        NgxEchartsModule.forRoot({ echarts: () => import('echarts') }) // Import NgxEchartsModule
     ]
 })
 export class ComponentsModule {}
