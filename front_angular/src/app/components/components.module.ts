2ca1e101f6b5deab7ca8b983320c652f9e31e06a
Lesmes <20rasputin03@gmail.com>
2024-08-13 21:28:38 -0500

    CREATE: graphics for specific repository

--- front_angular/src/app/components/components.module.ts
+++ front_angular/src/app/components/components.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { IonicModule } from '@ionic/angular'; // Import IonicModule
+import { NgxEchartsModule } from 'ngx-echarts'; // Import NgxEchartsModule
 
 import { FrontendGraphicComponentComponent } from "./frontend-graphic-component/frontend-graphic-component.component";
 import { BackendGraphicComponentComponent } from "./backend-graphic-component/backend-graphic-component.component";
 import { DatabaseGraphicComponentComponent } from "./database-graphic-component/database-graphic-component.component";
 import { EchartsComponent } from "./echarts/echarts.component";
-import { CircleDataComponent } from "./circle-data/circle-data.component"
-import { BarDataComponent } from './bar-data/bar-data.component'
+import { CircleDataComponent } from "./circle-data/circle-data.component";
+import { BarDataComponent } from './bar-data/bar-data.component';
 
 @NgModule({
     declarations: [
@@ -29,6 +30,7 @@ import { BarDataComponent } from './bar-data/bar-data.component'
     imports: [
         CommonModule,
         IonicModule,
+        NgxEchartsModule.forRoot({ echarts: () => import('echarts') }) // Import NgxEchartsModule
     ]
 })
 export class ComponentsModule {}
