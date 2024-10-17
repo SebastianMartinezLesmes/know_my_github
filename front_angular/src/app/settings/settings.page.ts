e64d95a5b3670865c225aaa21e9d1376ec07f30d
Lesmes <20rasputin03@gmail.com>
2024-10-16 20:39:11 -0500

    CREATE: system for put a dark mode

--- front_angular/src/app/settings/settings.page.ts
+++ front_angular/src/app/settings/settings.page.ts
@@ -8,19 +8,32 @@ import { Router } from '@angular/router';
 })
 export class SettingsPage implements OnInit {
 
+  iconName = 'sunny'; // Nombre del ícono por defecto
+
   constructor(
-    private router:Router
+    private router: Router
   ) { }
 
   ngOnInit() {
+    const isDarkMode = localStorage.getItem('dark-theme') === 'true';
+    this.iconName = isDarkMode ? 'moon' : 'sunny';
+    if (isDarkMode) {
+      document.body.classList.add('dark-theme');
+    }
   }
 
-  goGitHub(){
-    this.router.navigate(['./git-hub'])
-  }
+  goGitHub() { this.router.navigate(['./git-hub']); }
+  goHome() { this.router.navigate(['./home']); }
 
-  goHome() {
-    this.router.navigate(['./home']);
+  toggleTheme(event: CustomEvent) {
+    if (event.detail.checked) {
+      document.body.classList.add('dark-theme');
+      localStorage.setItem('dark-theme', 'true');
+      this.iconName = 'moon';
+    } else {
+      document.body.classList.remove('dark-theme');
+      localStorage.setItem('dark-theme', 'false');
+      this.iconName = 'sunny';
+    }
   }
-
 }
